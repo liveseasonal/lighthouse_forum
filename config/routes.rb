cef7c822a951f41cda6113507a011ac59c924562
liveseasonal <matt@liveseasonal.com>
2016-06-21 01:13:46 +0000

    Refactored to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 LighthouseForum::Application.routes.draw do
 
-  get 'posts',          to: 'posts#index'
-  get 'posts/new',      to: 'posts#new'
-  get 'posts/:id/edit', to: 'posts#edit'
-  get 'posts/:id',      to: 'posts#show', as: 'post' # necessary for the update action!
-  patch 'posts/:id',    to: 'posts#update'
-  post 'posts',         to: 'posts#create'
+
+
+  resources :posts
+#Refactored the below. Because so common rails gives us a method to run
+  # get 'posts',          to: 'posts#index'
+  # get 'posts/new',      to: 'posts#new'
+  # get 'posts/:id/edit', to: 'posts#edit'
+  # get 'posts/:id',      to: 'posts#show', as: 'post' # necessary for the update action!
+  # patch 'posts/:id',    to: 'posts#update'
+  # post 'posts',         to: 'posts#create'
 
   # get 'posts/0', to: 'posts#post0'
   # get 'posts/1', to: 'posts#post1'
